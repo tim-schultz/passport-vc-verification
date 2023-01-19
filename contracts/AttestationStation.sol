988e32b5128b6fcf34d5e0e915e7b8642fbc5257
schultztimothy <schultz.timothy52@gmail.com>
2023-01-19 13:27:50 -0700

    remove console

--- contracts/AttestationStation.sol
+++ contracts/AttestationStation.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4;
-import "hardhat/console.sol";
 
 contract AttestationStation {
     mapping(address => mapping(address => mapping(bytes32 => bytes))) public attestations;
@@ -17,9 +16,7 @@ contract AttestationStation {
         for (uint256 i = 0; i < _attestations.length; ++i) {
             AttestationData memory attestation = _attestations[i];
             attestations[msg.sender][attestation.about][attestation.key] = attestation.val;
-            // console.log(attestations, "attestations[msg.sender][attestation.about][attestation.key]");
-            console.log(attestation.about, "attestation.about");
-            console.log(msg.sender, "msg.sender");
+
             emit AttestationCreated(msg.sender, attestation.about, attestation.key, attestation.val);
         }
     }
